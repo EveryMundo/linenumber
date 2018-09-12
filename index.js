d56cc9616acdadcbb4950f558dee3cb0e6ae8825
Daniel Santana <github@danielsantana.net>
2018-09-12 14:46:22 -0400

    adds support for windows

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function linenumber() {
     { stack } = err,
     firstI = stack.indexOf('\n', stack.indexOf('\n') + 1),
     secndI = (stack + '\n').indexOf('\n', firstI + 1),
-    firstMatch = stack.substring(firstI, secndI).match(/\s+\(?(\/([^:]+):\d+)/);
+    line = stack.substring(firstI, secndI),
+    firstMatch = line.match(/\s+\(?((?:\/|\w:)([^:]+):\d+)/);
 
   return firstMatch[1].split(':').pop();
 }
